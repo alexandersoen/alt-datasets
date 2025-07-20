82e1532fe69ebaaeb2e88bc77caa29c3626073f8
Alexander Soen <alexandersoen@gmail.com>
2025-07-20 14:55:25 +0900

    Fix dog selection

--- build_all.sh
+++ build_all.sh
@@ -2,3 +2,5 @@
 
 tfds build --file_format=array_record cifar100_noisy/
 tfds build --file_format=array_record cifar100_longtail/
+
+tfds build --file_format=array_record --datasets=imagenet2012 --manual_dir=/data/giil/tensorflow_datasets/downloads/manual
