6fcf479a9ec3e4cbca094610cecca41c03d4655d
Alexander Soen <alexandersoen@gmail.com>
2025-07-20 15:50:47 +1000

    Remove debug print

--- imagenet2012_dogs/imagenet2012_dogs_dataset_builder.py
+++ imagenet2012_dogs/imagenet2012_dogs_dataset_builder.py
@@ -56,7 +56,6 @@ class NonDogDownSampler:
 
     def is_dog(self, record) -> bool:
         file_name = record["file_name"].decode("utf-8")
-        print(file_name)
         synset_id_str, *_ = file_name.split("_")
         synset_id = int(synset_id_str[1:])
 
