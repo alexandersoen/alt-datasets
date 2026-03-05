8230e8ea15cdc8817974740c3f7aa0ac259f85b1
Alexander Soen <alexandersoen@gmail.com>
2026-03-05 17:04:59 +0100

    Update cifar100 datasets

--- build_all.sh
+++ build_all.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-tfds build --file_format=array_record cifar100_noisy/
-tfds build --file_format=array_record cifar100_longtail/
+tfds build cifar100_noisy/
+tfds build cifar100_longtail/
 
-tfds build --file_format=array_record --datasets=imagenet2012 --manual_dir=/data/giil/tensorflow_datasets/downloads/manual
+# tfds build --file_format=array_record cifar100_noisy/
+# tfds build --file_format=array_record cifar100_longtail/
+
+# tfds build --file_format=array_record --datasets=imagenet2012 --manual_dir=/data/giil/tensorflow_datasets/downloads/manual
--- check.py
+++ check.py
@@ -6,23 +6,23 @@ import tensorflow_datasets as tfds
 
 
 def generate_hist(builder_str: str, split_str: str = "train") -> list[int]:
-    builder = tfds.builder(builder_str)
-    dataset = builder.as_dataset(split=split_str)
+  builder = tfds.builder(builder_str)
+  dataset = builder.as_dataset(split=split_str)
 
-    hist = [0] * 100
-    for example in dataset.as_numpy_iterator():  # pyright: ignore
-        hist[int(example["label"])] += 1
+  hist = [0] * 100
+  for example in dataset.as_numpy_iterator():  # pyright: ignore
+    hist[int(example["label"])] += 1
 
-    return hist
+  return hist
 
 
 if __name__ == "__main__":
-    print("Label Noise")
-    print(generate_hist("cifar100_noisy/label_noise_0"))
-    print(generate_hist("cifar100_noisy/label_noise_10"))
-    print(generate_hist("cifar100_noisy/label_noise_25"))
-    print()
-    print("Longtail")
-    print(generate_hist("cifar100_longtail/head_100"))
-    print(generate_hist("cifar100_longtail/head_50"))
-    print(generate_hist("cifar100_longtail/head_25"))
+  print("Label Noise")
+  print(generate_hist("cifar100_noisy/label_noise_0"))
+  print(generate_hist("cifar100_noisy/label_noise_10"))
+  print(generate_hist("cifar100_noisy/label_noise_25"))
+  print()
+  print("Longtail")
+  print(generate_hist("cifar100_longtail/head_100"))
+  print(generate_hist("cifar100_longtail/head_50"))
+  print(generate_hist("cifar100_longtail/head_25"))
